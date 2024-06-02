6b0be899487a8b7121f61096365050a698b5b331
codedcup <codedcup@gmail.com>
2024-06-03 01:22:40 +0530

    handled Negative Numbers input

--- test_app.py
+++ test_app.py
@@ -16,6 +16,15 @@ class TestStringCalculator(unittest.TestCase):
         
     def test_new_lines_between_numbers(self):
         self.assertEqual(add("1\n2,3"), 6)
+        
+    def test_custom_delimiters(self):
+        self.assertEqual(add("//;\n1;2;3"), 6)
+        self.assertEqual(add("//K\n1K2K3"), 6)
+        
+    def test_negative_numbers(self):
+        with self.assertRaises(ValueError) as context:
+            add("1,-2")
+        self.assertEqual(str(context.exception), "negative numbers not allowed")
 
 if __name__ == "__main__":
     unittest.main()
