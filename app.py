c2f0ae54a3fbea2868b864a89fec999011c0e707
codedcup <codedcup@gmail.com>
2024-06-03 01:17:11 +0530

    handled Multiple Numbers input

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 def add(numbers):
     if numbers == "":
         return 0
+    nums = map(int, numbers.split(","))
+    return sum(nums)
--- test_app.py
+++ test_app.py
@@ -4,6 +4,15 @@ from app import add;
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(add(""), 0)
+        
+    def test_one_number(self):
+        self.assertEqual(add("1"), 1)
+        
+    def test_two_numbers(self):
+        self.assertEqual(add("1,2"), 3)
+        
+    def test_multiple_numbers(self):
+        self.assertEqual(add("1,2,3,4"), 10)
 
 if __name__ == "__main__":
     unittest.main()
